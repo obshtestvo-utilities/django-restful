3c1cd2841f8ceba0bfb82b0109bd5b5938f3dfaf
Anton Stoychev <antitoxic@gmail.com>
2015-08-25 20:35:13 +0300

    Unpack single entry error lists into just the entry

--- restful/exception/verbose.py
+++ restful/exception/verbose.py
@@ -7,7 +7,13 @@ class VerboseException(Exception):
         self.errors = {}
 
     def get_errors(self):
-        return self.errors
+        compact_errors = {}
+        for topic, errors in self.errors.items():
+            if len(errors) == 1:
+                compact_errors[topic] = errors[0]
+            else:
+                compact_errors[topic] = errors
+        return compact_errors
 
     def set_errors(self, errors):
         """
--- restful/shortcuts.py
+++ restful/shortcuts.py
@@ -8,12 +8,6 @@ def errors(request):
     for message in get_messages(request):
         if message.level == constants.ERROR:
             errors_by_topic = dict(errors_by_topic, **json.loads(message.message))
-    for topic, errors in errors_by_topic.items():
-        try:
-            if len(errors) == 1:
-                errors_by_topic[topic] = errors[0]
-        except:
-            pass
 
     return errors_by_topic
 
